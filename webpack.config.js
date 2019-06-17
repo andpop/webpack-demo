ea19a44e078da2f2aea14436ed7cdb4230e5abef
Andrey Popov <andpop@mail.ru>
2019-06-18 00:17:18 +0300

    Lesson 7

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,12 @@ const conf = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename : 'main.js'
+        filename : 'main.js',
+        publicPath: 'dist/'
     }
+    // devServer: {
+    //     contentBase: './dist'
+    // }
 }
 
 module.exports = conf;
